f0b077fb2bec1a068c046468603c5079820a65d5
MAGNAT\ravshan_mu <muradov.rv@magnat.ru>
2023-01-26 15:57:37 +0300

    Add sources

---
+++ src/tree.js
@@ -0,0 +1,29 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+(async (pathToDir) => {
+  const dirnameByPath = path.dirname(pathToDir);
+  const tree = { files: [], folders: [] };
+  const growTree = async (p) => {
+    if (path.extname(p)) {
+      tree.files.push(path.relative(dirnameByPath, p));
+      return;
+    }
+
+    tree.folders.push(path.relative(dirnameByPath, p));
+
+    try {
+      const dirContents = await fs.readdir(p);
+
+      for (const content of dirContents) {
+        await growTree(`${p}\\${content}`);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  await growTree(pathToDir);
+
+  return tree;
+})(process.argv[2]).then((res) => console.log(res));
